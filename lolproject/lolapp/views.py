24cecc4dfceba659fca004bbd92762007c5087dd
Nathaniel Sop <nathanielsop@gmail.com>
2021-12-04 11:51:17 -0500

    BE Logic Side Commit

--- lolproject/lolapp/views.py
+++ lolproject/lolapp/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
+import os
 import requests
 import json
 from urllib.request import urlopen
+from dotenv import load_dotenv
+
+
+# Load the .env file
+load_dotenv()
 
 
 # Create your views here.
 def home(request):
-    # api_key = "RGAPI-3a58d94f-8442-4f65-a3a6-966b44cffaba"
-    # username = ''
-    # response = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ username +'?api_key='+ api_key)
-    # json_response = response.json()
-    # encid = json_response['id']
-
     return render(request, './index.html')
 
 def search(request):
     if request.method == "POST":
         # User searches for name and gets their encrypted ID
         userName = request.POST.get('username')
-        api_key = "RGAPI-006b93e8-0346-49ae-8749-ed753a0547b6"
+        api_key = os.getenv("API_KEY")
 
         response = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+ userName +'?api_key='+ api_key)
         json_response = response.json()
@@ -26,20 +26,32 @@ def search(request):
 
         # Load in the champion data
         url = 'http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion.json'
-        champ_response = urlopen(url)
-        champ_data = json.loads(champ_response.read())
+        champ_response = requests.get(url)
+        champ_data = json.loads(champ_response.text)
+        champion_id = champ_data['data']
+        
+        # Part of Champion Data, sort through the JSON and fill championDict var with keys and ids of champions
+        champion_ids = []
+        champion_names = []
+        for champName, champDict in champion_id.items():
+            for (champName, value) in champDict.items():
+                newDict = {"name": champName, "value": value}
+                if newDict['name'] == 'key':
+                    champion_ids.append(newDict[('value')])
+                if newDict['name'] == 'id':
+                    champion_names.append(newDict[('value')])
+
+        champion_ids = [int(n) for n in champion_ids]            
+
+        zip_champs = zip(champion_names, champion_ids)
+
+        championDict = dict(zip_champs)
 
         # Return the mastery data
         mResponse = requests.get('https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/'+ encid+'?api_key=' + api_key)
         mastery = json.loads(mResponse.text)
 
-        
-        # for key in champ_data:
-        #     for id in mastery:
-        #         if key == id:
-        #             return key['id']
-
-        return render(request, './index.html', {'mastery':mastery, 'userName': userName})  
+        return render(request, './index.html', {'mastery':mastery, 'userName': userName, 'champion_id':champion_id, 'championDict':championDict })  
 
     else:
     

aa3dff21d4d00f4e877591b9248e2b46433e16f2
Nathaniel Sop <nathanielsop@gmail.com>
2021-12-25 21:42:26 -0500

    Redirect Setup, Redis Caching Setup (#2)

--- lolproject/lolproject/urls.py
+++ lolproject/lolproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from lolapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('lolapp.urls'))
+    path('app/', include('lolapp.urls')),
+    path('', views.redirect_view)
 ]
